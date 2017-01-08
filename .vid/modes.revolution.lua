1242d2ea036b39db6824654756824b352bc2c99a
Martin Schrodt <martin@schrodt.org>
2017-01-08 15:27:14 +0100

    set gnome scaling factor and restart wm when changing mode

--- .vid/modes.revolution.lua
+++ .vid/modes.revolution.lua
@@ -13,28 +13,28 @@ return {
 			name = "single",
 			description = "single screen, high resolution",
 			cmd = "--output eDP-1 --auto --output HDMI-1 --off",
-			dpi = 192,
+			scale = 2,
 		},
 
 		{
 			name = "single-low",
 			description = "single screen, low resolution",
 			cmd = "--output eDP-1 --mode 1920x1080 --output HDMI-1 --off",
-			dpi = 96,
+			scale = 1,
 		},
 
 		{
 			name = "proway-dev",
 			description = "development @ proway",
 			cmd = "--output eDP-1 --mode 1920x1080 --pos 0x360 --output HDMI-1 --auto --pos 1920x0",
-			dpi = 96,
+			scale = 1,
 		},
 
 		{
 			name = "proway-beamer",
 			description = "beamer @ proway",
 			cmd = "--output eDP-1 --mode 1920x1080 --pos 0x1080 --output HDMI-1 --mode 1920x1080 --pos 0x0",
-			dpi = 96,
+			scale = 1,
 		},
 
 	},
